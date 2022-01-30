52d32295a4f97bfa46e4e7b45427808e02cdc0f3
Dayvyno2021 <dayvynomern2021@gmail.com>
2022-01-30 18:02:05 +0100

    body parse

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: false }))
 
 
 const __dirname = path.resolve()
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <div className=''>
+      <div>
+        <div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreen;
